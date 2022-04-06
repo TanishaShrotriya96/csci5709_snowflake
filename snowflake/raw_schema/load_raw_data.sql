3b350cca984dccda23d9d83b69c2c8a861226143
TanishaShrotriya96 <tanishashrotriya91@gmail.com>
2022-04-06 14:28:47 -0500

    changed from .*Customers[1-9]*.csv to .*Customers.*.csv

--- snowflake/raw_schema/load_raw_data.sql
+++ snowflake/raw_schema/load_raw_data.sql
@@ -14,17 +14,17 @@ list @sales_stage;
 
 copy into customers
     from @sales_stage/customers/
-    pattern='.*Customers[1-9]*.csv'
+    pattern='.*Customers.*.csv'
     on_error = 'skip_file';
 
 copy into products
     from @sales_stage/products/
-    pattern='.*Products[1-9]*.csv'
+    pattern='.*Products.*.csv'
     on_error = 'skip_file';
 
 copy into sales
     from @sales_stage/sales/
-    pattern='.*Sales[1-9]*.csv'
+    pattern='.*Sales.*.csv'
     on_error = 'skip_file';
 
 create or replace file format employee_csv_format
@@ -38,5 +38,5 @@ create or replace stage employees_stage
 
 copy into employees
     from @employees_stage/employees/
-    pattern='.*Employees[1-9]*.csv'
+    pattern='.*Employees.*.csv'
     on_error = 'continue';
